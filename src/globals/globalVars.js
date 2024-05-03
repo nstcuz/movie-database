2593e043bb414bfbdb326edaa44980ae3e200123
Catharina <144069315+cgtrust@users.noreply.github.com>
2024-05-03 14:37:50 -0700

    updated globals folder to include api links

---
+++ src/globals/globalVars.js
@@ -0,0 +1,4 @@
+export const appTitle = 'Cheeseboard Cinema';
+export const appStorageName = 'movies';
+export const movieEndpoint = 'https://api.themoviedb.org/3/movie/';
+export const movieSearch = 'https://api.themoviedb.org/3/search/movie?query=';
