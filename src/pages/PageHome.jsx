5b5ebf239bf5c39eb4e79eca997ed37bb14398d1
Natalia N. <156234685+nat-my-nguyen@users.noreply.github.com>
2024-05-03 13:38:54 -0700

    Initial work on home page

---
+++ src/pages/PageHome.jsx
@@ -0,0 +1,15 @@
+const PageHome = () => {
+    return (
+        <main>
+            <section>
+                <p>Slideshow goes here</p>
+            </section>
+			<section>
+				<p>Tab between Popular/Now Playing/Upcoming/Top Rated thumbs.</p>
+                <p>Thumbs go here.</p>
+			</section>
+		</main>
+    )
+}
+
+export default PageHome;
