248c08dcb98911ef1dec3a649a48c22a5a72647b
nstcuz <126234425+nstcuz@users.noreply.github.com>
2024-05-06 14:32:48 -0700

    started populating page with data && created _single-details.scss to start styling

--- src/pages/PageSingleDetail.jsx
+++ src/pages/PageSingleDetail.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { movieEndpoint } from '../globals/globalVars';
 const apiKey = import.meta.env.VITE_MOVIEDB_API_KEY;
-
+// import Header from '../components/Header';
+// import Footer from '../components/Footer';
 
 function App() {
-const [movie, setMovie] = useState(null);
-const [loading, setLoading] = useState(false);
-const [error, setError] = useState('');
+  const [movie, setMovie] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
   useEffect(() => {
     const fetchMovie = async () => {
-        setLoading(true);
+      setLoading(true);
       try {
-        const res= await fetch(movieEndpoint + '112?api_key=' + apiKey);
+        const res = await fetch(movieEndpoint + '112?api_key=' + apiKey);
         const data = await res.json();
         console.log(data);
         setMovie(data);
@@ -27,13 +29,33 @@ const [error, setError] = useState('');
   }, []);
 
   return (
-    <div className="movie-card">
-      <h2>{movie ? movie.title: 'movie title not available'}</h2>
-      { movie ? <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} /> : 'sorry the movie hasnt loaded yet' }
-      {loading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
+    <div>
+      {/* <Header /> */}
+      {movie && (
+        <section>
+          <h2>{movie.title}</h2>
+          <p>{movie.release_date}</p>
+          <div>
+            <p>{movie.vote_average}</p>
+                {movie.genres && (
+                    <ul>
+                    {movie.genres.map((genre) => (
+                        <li key={genre.id}>{genre.name}</li>
+                    ))}
+                    </ul>
+                )}
+            <img src="../images/cheese-heart.svg" alt="Cheese Heart" />
+          </div>
+        </section>
+      )}
+      <section className="movie-card">
+        { movie ? <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} /> : 'Sorry, the movie has not loaded yet' }
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </section>
+      {/* <Footer /> */}
     </div>
-  )
+  );
 }
 
 export default App;
