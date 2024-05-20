20830ab580c36a8c413c936bb649f1d4ab43d8d6
mlew02 <matthewjameslew@gmail.com>
2024-05-20 11:34:42 -0700

    updated css

--- src/pages/PageFavorites.jsx
+++ src/pages/PageFavorites.jsx
@@ -1,57 +1,55 @@
-import '../scss/styles.scss'
-import { Link } from 'react-router-dom';
-import { truncateTitle, truncateOverview, formatRatingPercentage, appTitle }  from '../globals/globalVars';
-import { useSelector } from 'react-redux';
-import Thumbnail from '../components/Thumbnail';
-import isFav from '../../utilities/isFav.js';
-// import { truncateTitle, truncateOverview, formatRatingPercentage } from '../globals/globalVars';
-import {useEffect} from 'react'; 
+// import "../scss/styles.scss";
+import { Link } from "react-router-dom";
+import {
+  truncateTitle,
+  truncateOverview,
+  formatRatingPercentage,
+  appTitle
+} from "../globals/globalVars";
+import { useSelector } from "react-redux";
+import Thumbnail from "../components/Thumbnail";
+import isFav from "../../utilities/isFav.js";
+import { useEffect } from "react";
 
 
-function PageFavs() {
 
-    const favs = useSelector((state) => state.favs.items);
-    console.log(favs);
+function PageFavorites() {
+  const favs = useSelector((state) => state.favs.items);
+  console.log(favs);
 
-    // useEffect(() => {
-    //     const fetchMovies = async () => {
-    //       const response = await fetch(`${movieEndpoint}?api_key=${apiKey}`);
-    //       let data = await response.json();
-    //       setMovies(data);
-    //       console.log(data);
-    //     };
+  useEffect(() => {
+    document.title = `${appTitle}-favs`;
+  });
 
-    //     fetchMovies();
-    // }, []);
-    useEffect(() =>{
-        document.title = `${appTitle}-favs`
-
-    })
-
-
-    return (
-        <main>
-		    <section>
-                <h2>Favourite Movies</h2>
-                {favs.length < 1 ? <p>No favourite movies. Return to the <Link to="/">home</Link> page to add some favourite kittens.</p> : 
-                <section className="thumbnail-section">
-                {favs != null && favs.length > 0 && favs.map(movie => (
-                    <Thumbnail
-                        key={movie.id}
-                        title={truncateTitle(movie.title, 32)}
-                        release_date={movie.release_date}
-                        overview={truncateOverview(movie.overview, 12)}
-                        rating={formatRatingPercentage(movie.vote_average)}
-                        image={movie.poster_path}
-                        isFav={isFav(favs, null, movie.id)}
-                        
-                    />
+  return (
+    <main className="fav-container">
+      <section>
+        <div className="heading-fav">
+          <h2>Favourite Movies</h2>
+        </div>
+        <div className="content">
+          {favs.length < 1 ? (
+            <p className="fav-p">
+              No favourite movies. Return to the <Link to="/">home</Link> page to
+              add some favourite movies.
+            </p>
+          ) : (
+            <section className="thumbnail-section">
+              {favs != null &&
+                favs.length > 0 &&
+                favs.map((movie) => (
+                  <Thumbnail
+                    key={movie.id}
+                    movie={movie}
+                    isFav={isFav(favs, true, movie.id)}
+                  />
                 ))}
-			</section>}
             </section>
-	    </main>
-    );
-	
+          )}
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default PageFavs;
+export default PageFavorites;
