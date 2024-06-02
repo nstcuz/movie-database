92cc4c50c1f96d8aa51b73c5d591a268ca7f871f
mlew02 <matthewjameslew@gmail.com>
2024-06-02 16:12:49 -0700

    more comments in about and fav pages

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -8,20 +8,25 @@ const About = () => {
             <div className='about-container'>
                 <div className='header-container about-container'>
                 <div className='clacker'>
+                     {/* heading */}
                     <h2>About Us</h2>
                 </div>
                 </div>
+                 {/* About the movie text */}
             <div className='about-text'>
             <p>Cheeseboard Cinema is a movie database where users can search up movie titles based on different catagories, such as genre and popularity.</p>
+             {/* more text in the about and a link to the favouries page  */}
             <p className="border-bottom">Join and build your collect with our <NavLink to="/favorites">Favorites</NavLink> feature!</p>
             <p>So... what's with the <em>"cheese"</em> theme?</p>
             <p>Ever watched something and thought <em>"That's so cheesey"</em>?</p>
             <p className="border-bottom">The creator behind Cheeseboard Cinema combined that sentiment and the iconic "clapperboard", which a "cheeseboard" movie database
                 where everyone can create their own list of movies catered to them!
             </p>
+             {/* links to every members page and copyright */}
             <p>Project collaboration with <a href='https://www.cgtwebdesigns.com/' className='tolower'>Catharina Trust</a>, <a href='https://www.natcreates.com/' className='tolower'>Natalia Nguyen</a>, <a href='https://mlewebs.ca/' className='tolower'>Matthew Lew</a> and <a href='https://niccousins.com/' className='tolower'>Nic Cousins</a>.</p>
             <p>This product uses the <a href="https://developer.imdb.com/">TMDb API</a> but is not endoresed or certified by TMDb.</p>
             <div className='image-container'>
+                 {/* TMDB logo*/}
             <img src={logo} alt="TMDb logo" />
             </div>
             </div>
--- src/pages/PageFavorites.jsx
+++ src/pages/PageFavorites.jsx
@@ -26,25 +26,29 @@ function PageFavorites() {
     <main>
       <section className="fav-container">
         <div className="heading-fav">
+           {/* heading*/}
           <h2>Favourite Movies</h2>
         </div>
         <div className="content">
+           {/* Checks if there are faved movies*/}
           {favs.length < 1 ? (
             <div className="paragraph-container">
               <div className="dashed-border">
                 <p className="fav-p">
+                  {/* if there are no favs it displays this  */}
                   That's not <em>gouda</em>, you have no favourites!
                 </p>
                 <p className="fav-p">
+                  {/* if there are no favs it displays this also */}
                   Return to the <Link to="/">home</Link> page and start your collection by clicking the hearts on your favourite movies
                 </p>
               </div>
             </div>
           ) : (
-            <section className="thumbnail-section">
+            <section className="thumbnail-section"> {/* Map over favorite movies which looks at each unique key, passes the movie data to the thumbnail component and cehcks if the movie is faved */}
               {favs.map((movie) => (
                 <Thumbnail
-                  key={movie.id}
+                  key={movie.id}  
                   movie={movie}
                   isFav={isFav(favs, true, movie.id)}
                 />
